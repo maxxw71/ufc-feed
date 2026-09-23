6d16e4a4e5024bd40ceed589575d8ba443cea4bf
maxxw71 <maxxw@tutanota.com>
2026-09-23 16:01:15 -0400

    Test archived punch availability dating

--- boxing/research/test_archived_compubox_summaries.py
+++ boxing/research/test_archived_compubox_summaries.py
@@ -1,5 +1,5 @@
 import unittest
-from export_archived_compubox_summaries import parse_summary_metrics
+from export_archived_compubox_summaries import parse_summary_metrics,archive_date
 
 class ArchivedCompuBoxSummaryTests(unittest.TestCase):
     def test_garcia_burgos_current_fight_rates_only(self):
@@ -20,6 +20,10 @@ class ArchivedCompuBoxSummaryTests(unittest.TestCase):
         self.assertEqual(out['Artur Szpilka']['total_accuracy_pct'],24.0)
         self.assertEqual(out['Bryant Jennings']['total_thrown_per_round'],42.0)
         self.assertEqual(out['Bryant Jennings']['power_accuracy_pct'],46.0)
+
+    def test_wayback_capture_date(self):
+        self.assertEqual(archive_date('https://web.archive.org/web/20151208081612/http://compuboxonline.com/x/'),'2015-12-08')
+        self.assertIsNone(archive_date('https://compuboxonline.com/x/'))
 
 if __name__=='__main__':
     unittest.main()
