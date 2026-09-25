593d89fee69d30f9f4abe05739dc2f59aebad7ce
maxxw71 <maxxw@tutanota.com>
2026-09-25 09:01:20 -0400

    Test exact Wayback replay pinning

--- boxing/research/test_historical_odds_snapshot.py
+++ boxing/research/test_historical_odds_snapshot.py
@@ -1,11 +1,21 @@
 import unittest
-from validate_historical_odds_snapshot import american_to_decimal,parse_snapshot
+from validate_historical_odds_snapshot import american_to_decimal,parse_snapshot,snapshot_variants,wayback_timestamp
 
 class HistoricalOddsArchiveValidationTests(unittest.TestCase):
     def test_american_decimal_conversion(self):
         self.assertAlmostEqual(american_to_decimal('+140'),2.4)
         self.assertAlmostEqual(american_to_decimal('-200'),1.5)
         self.assertIsNone(american_to_decimal('n/a'))
+
+    def test_wayback_snapshot_variants_stay_on_requested_timestamp(self):
+        url='https://web.archive.org/web/20210528114306id_/https://www.proboxingodds.com/events/2021-05-29-1344'
+        variants=snapshot_variants(url)
+        self.assertGreaterEqual(len(variants),8)
+        self.assertTrue(all('/web/20210528114306' in x for x in variants))
+        self.assertTrue(any('/http://www.proboxingodds.com/' in x for x in variants))
+        self.assertTrue(any('/https://proboxingodds.com/' in x for x in variants))
+        self.assertEqual(wayback_timestamp(url),'20210528114306')
+        self.assertEqual(wayback_timestamp('https://web.archive.org/web/20210119154525id_/x'),'20210119154525')
 
     def test_exact_book_bout_selection_cell(self):
         html=b'''<table class="odds-table"><thead><tr><th></th><th></th><th data-b="20">BetWay</th></tr></thead>
